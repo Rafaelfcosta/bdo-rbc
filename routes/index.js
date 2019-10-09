1a770c29cb859da81abcd95fbe85caae7c4d5b51
eugeniaaf <eugenia.af@icloud.com>
2019-10-09 14:38:47 -0300

    Commit
    Geral
       * Implementado front-end.
       * Corrigido calculo de similaridade.

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    let casetest = {
+    let testCase = {
         "EstiloDeJogo": req.body.EstiloDeJogo,
         "ObjetivoDeJogo": req.body.ObjetivoDeJogo,
         "Dificuldade": req.body.Dificuldade,
@@ -17,45 +17,36 @@ router.post('/', (req, res) => {
         "Prioridade": req.body.Prioridade
     };
 
+    let weights = {
+        "EstiloDeJogo": +req.body.EstiloDeJogo_peso,
+        "ObjetivoDeJogo": +req.body.ObjetivoDeJogo_peso,
+        "Dificuldade": +req.body.Dificuldade_peso,
+        "TipodeDano": +req.body.TipodeDano_peso,
+        "Prioridade": +req.body.Prioridade_peso
+    };
+
     cases.forEach(caso => {
-        let somatorio = 0;
+        let somatorio = 0.0;
         for (const att in attr) {
             if (attr.hasOwnProperty(att)) {
                 const at = attr[att];
-                let res = sim(at.valores[caso[att]], at.valores[casetest[att]], at.peso);
+                let res = sim(at.valores[testCase[att]], at.valores[caso[att]], weights[att]);
                 somatorio += res;
             }
         }
-        let percent = (somatorio / getPesoTotal()) * 100;
-        caso.similaridade = parseInt(percent);
+
+        let percent = (somatorio / getPesoTotal(weights)) * 100.0;
+
+        caso.similaridade = parseFloat(percent).toFixed(2);
     });
 
-    sort_by_key(cases, 'similaridade');
-
+    sortByKey(cases, 'similaridade');
 
     res.render('result', {resultCases: cases, attributes: attr});
 });
 
-let sim = function (at1, at2, peso) {
-    return (1 - Math.abs(at1 - at2)) * peso;
-};
-
-let getPesoTotal = function () {
-    let sum = 0;
-    for (const att in attr) {
-        if (attr.hasOwnProperty(att)) {
-            const at = attr[att];
-            sum += at.peso;
-        }
-    }
-    return sum
-};
-
-function sort_by_key(array, key) {
-    return array.sort(function (a, b) {
-        var x = a[key]; var y = b[key];
-        return ((x > y) ? -1 : ((x < y) ? 1 : 0));
-    });
-}
+const sim = (at1, at2, peso) => (1.0 - Math.abs(at2 - at1)) * peso;
+const getPesoTotal = (pesos) => Object.values(pesos).reduce((a, b) => a + b, 0.0);
+const sortByKey = (array, key) => array.sort((a, b) => b[key] - a[key]);
 
 module.exports = router;
